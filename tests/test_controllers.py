4dab8e3d62b3bd1aefc5e456af007ab24f84a9f5
Vikas Dhiman <wecacuee@users.noreply.github.com>
2021-01-04 11:03:48 -0800

    disable pytest for now

--- tests/test_controllers.py
+++ tests/test_controllers.py
@@ -3,7 +3,12 @@ import torch
 from bayes_cbf.controllers import SOCPController, SumDynamicModels
 from bayes_cbf.planner import PiecewiseLinearPlanner
 from bayes_cbf.misc import to_numpy, random_psd
-from bayes_cbf.unicycle import RelDeg1CLF, ShiftInvariantModel, UnicycleDynamicsModel
+from bayes_cbf.unicycle_move_to_pose import (ControllerCLFBayesian,
+                                             PiecewiseLinearPlanner,
+                                             LearnedShiftInvariantDynamics,
+                                             CLFCartesian,
+                                             obstacles_at_mid_from_start_and_goal,
+                                             AckermanDrive)
 from bayes_cbf.cbc2 import cbc2_quadratic_terms
 
 def test_convert_cbc_terms_to_socp_term(m = 2,
@@ -29,20 +34,31 @@ def test_convert_cbc_terms_to_socp_term(m = 2,
 def test_cbc2_quadratic_terms():
     m = 2
     n = 3
-    x_d = torch.rand(n)
+    x_g = torch.rand(n)
     u0 = torch.rand(m)
     x = torch.rand(n)
-    net_model = SumDynamicModels(
-        ShiftInvariantModel(
-            n, m),
-        UnicycleDynamicsModel())
-    clf = RelDeg1CLF(net_model,
-                     planner=PiecewiseLinearPlanner(x0 = x, x_goal =
-                                                    x_d, numSteps = 3,
-                                                    dt = 0.1))
+    dt = 0.01
+    cbf_gammas = [5., 5.]
+    Kp = [0.9, 1.5, 0.]
+    numSteps = 200
+    clf = CLFCartesian(Kp = torch.tensor(Kp))
+    ctrller = ControllerCLFBayesian(
+            PiecewiseLinearPlanner(x, x_g, numSteps, dt),
+            coordinate_converter = lambda x, x_g: x,
+            dynamics = LearnedShiftInvariantDynamics(dt = dt,
+                                                     mean_dynamics = AckermanDrive()),
+            clf = clf,
+            cbfs = obstacles_at_mid_from_start_and_goal(x , x_g),
+            cbf_gammas = torch.tensor(cbf_gammas)
+
+    )
+    state = x
+    state_goal = x_g
+    t = 20
     (bfe, e), (V, bfv, v), mean, var = cbc2_quadratic_terms(
-        lambda u: clf.clc(0, u), x, u0)
-    assert to_numpy(bfe @ u0 + e) == pytest.approx(to_numpy(clf.clc(0, u0).mean(x)), abs=1e-4, rel=1e-2)
+        lambda u: ctrller._clc(state, state_goal, u, t) * -1.0,
+        state, torch.rand(m))
+    assert to_numpy(bfe @ u0 + e) == pytest.approx(to_numpy(ctrller._clc(x, x_g, u0, t)), abs=1e-4, rel=1e-2)
 
 
 if __name__ == '__main__':
